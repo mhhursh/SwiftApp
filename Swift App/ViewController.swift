71aed75223aaf557967a00899bbe6ca2b9a5fe8d
Mark Hursh <mark.hursh@gmail.com>
2017-05-28 15:43:29 -0400

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var state = 0
+    var counter = 0
 
+    @IBOutlet weak var countTapsLabel: UILabel!
+    @IBOutlet weak var theLabel: UILabel!
+    @IBOutlet weak var buttonLabel: UIButton!
+    @IBAction func pushMePressed(_ sender: Any) {
+        self.view.backgroundColor = UIColor.darkGray
+        theLabel.text = "Buttons are cool"
+        counter += 10
+        print(counter)
+        countTapsLabel.text = "Counter = \(counter)"
+        
+    }
+    @IBAction func updateButton(_ sender: Any) {
+        
+        if (state == 0) {
+            theLabel.text = "Hello Katie and Erin!"
+            buttonLabel.setTitle("Oh wow, you did it!", for: UIControlState.normal)
+            self.view.backgroundColor = UIColor.lightGray
+            state = 1
+        } else {
+            theLabel.text = "Hello World"
+            buttonLabel.setTitle("Push Me, I Dare you!", for: UIControlState.normal)
+            self.view.backgroundColor = UIColor.white
+            state = 0
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+
     }
 
     override func didReceiveMemoryWarning() {
